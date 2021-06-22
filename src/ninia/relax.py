889b26a7e4088a73b3ed48cba9df30dc2373d220
ajsummers <ajs0201@auburn.edu>
2021-06-22 18:34:14 -0500

    Muted false pseudo_dir exception - fix later

--- src/ninia/relax.py
+++ src/ninia/relax.py
@@ -89,8 +89,8 @@ class Relax:
 
     def get_species_info_(self, species_list, pseudodir=None):
 
-        if (self.pseudo_dir is None) and (pseudodir is None):
-            raise RuntimeError('Define pseudopotential directory before geometry.')
+        # if (self.pseudo_dir is None) and (pseudodir is None):
+        #     raise RuntimeError('Define pseudopotential directory before geometry.')
 
         os.chdir(pseudodir)
         list_upf = flt(os.listdir('.'), '*.[Uu][Pp][Ff]')
