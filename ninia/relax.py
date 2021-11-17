63255f2e9eab0fdd57c79bf592dd70a8da525be7
ajsummers <ajs0201@auburn.edu>
2021-11-17 16:28:12 -0600

    Added functionality to create ASE objects from QE output

--- ninia/relax.py
+++ ninia/relax.py
@@ -16,6 +16,8 @@ from fnmatch import filter as flt  # Native filter() function is used as well
 import pandas as pd
 import numpy as np
 
+from ase.build import hcp0001
+
 starting_dir = os.getcwd()
 
 # Pull in molar mass data from separate csv file
@@ -293,4 +295,32 @@ class Relax:
         else:
 
             print(f'Muted redundant bash file {self.prefix}.sh')
+
+
+def ase_from_ouput(file):
+
+    with open(file, 'r') as f:
+        handle = f.read()
+
+    match = r'(?<=ATOMIC_POSITIONS)(.*?)(?=(End final coordinates|\n\n))'
+    matches = re.findall(match, handle, flags=re.S)
+
+    element_list, x_list, y_list, z_list = [[], [], [], []]
+
+    last_match = matches[-1]
+    formatted = '\n'.join(last_match[0].split('\n')[1:])
+    num_atoms = len(formatted.split('\n')) - 1
+    for line in formatted.split('\n')[:-1]:
+        line_list = line.split()
+        element_list.append(line_list[0])
+        x_list.append(float(line_list[1]))
+        y_list.append(float(line_list[2]))
+        z_list.append(float(line_list[3]))
+
+    position_list = [tuple(group) for group in zip(x_list, y_list, z_list)]
+    ase_object = hcp0001('H', size=(num_atoms, 1, 1), a=2.7059, c=4.2815)
+    ase_object.set_chemical_symbols(element_list)
+    ase_object.set_positions(position_list)
+
+    return ase_object
 
