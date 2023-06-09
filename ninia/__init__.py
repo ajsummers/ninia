c8dd1542acc33490d1aa382e979321ee509b14e5
ajsummers <ajs0201@auburn.edu>
2023-06-09 15:58:31 -0500

    Updated SISSO.in.jinja2 to include original detail

--- ninia/__init__.py
+++ ninia/__init__.py
@@ -7,7 +7,7 @@ if sys.version_info[0] == 2:
     raise ImportError('Ninia requires Python 3.7. This is Python 2.')
 
 __all__ = ['Relax', 'parse_sisso_eqn', 'gen_sisso', 'run_sisso']
-__version__ = '0.0.83'
+__version__ = '0.0.84'
 
 from ninia.relax import Relax
 from ninia.sisso import gen_sisso, run_sisso
